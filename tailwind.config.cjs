f2dd59d675b73cecf5ca4a0227cbf0b76658fb65
codesh85 <codesh85@gmail.com>
2024-12-22 01:59:22 +0800

    feat: update primary color in Tailwind configuration

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,17 +4,19 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    colors: {
-      'primary': '#1619D3',
-      'on-primary': '#FFFFFF',
-      'secondary': '#B0D7FF',
-      'on-secondary': '#FFFFFF',
-      'success': '#139A43',
-      'on-success': '#FFFFFF',
-      'error': '#D63230',
-      'on-error': '#FFFFFF',
-      'surface': '#EEEEEE',
-      'current': 'currentColor',
+    extend: {
+      colors: {
+        'primary': '#0848a6',
+        'on-primary': '#FFFFFF',
+        'secondary': '#B0D7FF',
+        'on-secondary': '#FFFFFF',
+        'success': '#139A43',
+        'on-success': '#FFFFFF',
+        'error': '#D63230',
+        'on-error': '#FFFFFF',
+        'surface': '#EEEEEE',
+        'current': 'currentColor',
+      }
     }
   },
   plugins: [],
