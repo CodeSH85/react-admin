37c62980264c8ac230b90fd99653bc539348ec94
CodeSH85 <codesh85@gmail.com>
2025-04-02 23:23:39 +0800

    feat: Headless UI Menu component with dynamic item rendering

--- src/components/ui/menu/menu.tsx
+++ src/components/ui/menu/menu.tsx
@@ -1,24 +1,32 @@
-import type { Menu } from "./type"
-import ButtonComp from "../button/button"
+import { useState } from 'react';
+import { Menu as HMenu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { MenuPropType } from "./type"
 
-const MenuComp = (props: Menu) => {
-  const { items } = props
+const Menu = (props: MenuPropType) => {
+  const {
+    title,
+    items=[]
+  } = props;
+
   return (
-    <div>
-      <ButtonComp>
-        Menu
-      </ButtonComp>
-      <div className="">
-        <ul>
-          {
-            items.map(item => (
-              <li>{ item.title }</li>
-            ))
-          }
-        </ul>
-      </div>
-    </div>
+    <HMenu>
+      <MenuButton>{ title }</MenuButton>
+      <MenuItems anchor="bottom">
+        {
+          items?.map((item) => (
+            <MenuItem key={item.key}>
+              <a 
+                className="block data-[focus]:bg-blue-100" 
+                href={item.key}
+              >
+                {item.title}
+              </a>
+            </MenuItem>
+          ))
+        }
+      </MenuItems>
+    </HMenu>
   )
 }
 
-export default MenuComp
+export { Menu }
--- src/components/ui/menu/type.ts
+++ src/components/ui/menu/type.ts
@@ -1,9 +1,15 @@
+import type { MenuProps as HeadlessUIProp } from '@headlessui/react';
+
+export type MenuPropType = {
+  title?: string;
+  items?: MenuItem[]
+  closeOnSelect?: boolean
+  closeOnBlur?: boolean
+  [key: string]: any
+}
+
 export type MenuItem = {
   key: string
   title: string | number
   icon?: string
-}
-
-export interface Menu {
-  items: MenuItem[]
-}
+} & HeadlessUIProp
