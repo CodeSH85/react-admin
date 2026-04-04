47c054f80e2a86cc0bed0abe0fe4b882996a6702
CodeSH85 <codesh85@gmail.com>
2026-04-04 22:40:26 +0800

    fix: ts & vite config setting

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,18 @@
 import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
+import type { UserConfig } from 'vite'
 
 export default defineConfig({
+  resolve: {
+    tsconfigPaths: true
+  },
   server: {
     port: 3000
   },
   plugins: [
     tailwindcss(),
-    // Enables Vite to resolve imports using path aliases.
-    tsconfigPaths(),
     tanstackStart({
       srcDirectory: 'src', // This is the default
       router: {
@@ -22,4 +23,4 @@ export default defineConfig({
     }),
     viteReact()
   ]
-})
+}) satisfies UserConfig
