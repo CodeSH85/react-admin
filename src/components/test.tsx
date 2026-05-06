eb1d37bf90bbba3e909cbfd98b4378ca22a957c5
CodeSH85 <codesh85@gmail.com>
2026-05-07 00:40:32 +0800

    feat: Add basic select component

--- src/components/test.tsx
+++ src/components/test.tsx
@@ -1,19 +1,27 @@
 import { useState } from 'react'
 import { Button } from '@/components/ui/button'
 import { Dialog } from '@/components/ui/dialog'
-import { Checkbox } from './ui/checkbox'
+import { Checkbox } from '@/components/ui/checkbox'
+import { Select } from '@/components/ui/select'
 
 const formFields = {
-  isAgree: false
+  isDisabled: false,
+  isReadonly: false
 }
 
 export const TestComponent = () => {
   const [form, setForm] = useState(formFields)
 
-  function setAgree(value: boolean) {
+  function setDisable(value: boolean) {
     setForm({
       ...form,
-      isAgree: value
+      isDisabled: value
+    })
+  }
+  function setReadonly(value: boolean) {
+    setForm({
+      ...form,
+      isReadonly: value
     })
   }
 
@@ -36,12 +44,33 @@ export const TestComponent = () => {
       <div className="bg-bg-base-2 dark:bg-dark-bg-base-2 flex-1">
         Second Layer
       </div>
-      <Checkbox
-        label='Agree'
-        checked={form.isAgree}
-        onCheckedChange={setAgree}
-      >
-      </Checkbox>
+      <div className="flex items-center gap-md">
+        <Checkbox
+          label='Disable'
+          checked={form.isDisabled}
+          onCheckedChange={setDisable}
+        />
+        <Checkbox
+          label='Readonly'
+          checked={form.isReadonly}
+          onCheckedChange={setReadonly}
+        />
+      </div>
+
+      <div className="min-w-12">
+        <Select
+          disabled={form.isDisabled}
+          placeholder={'select'}
+          readonly={form.isReadonly}
+          items={[
+            { label: 'test', value: 'test' },
+            { label: 'test 2', value: 'test2', disabled: true },
+            { label: 'test 3', value: 'test4' },
+            { label: 'test 4', value: 'test5' }
+          ]}
+        >
+        </Select>
+      </div>
       <Dialog
         description='This is Dialog'
         title='Dialog Title'
